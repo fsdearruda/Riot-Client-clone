4043eae88a5407f152a079bebb037c7db3d895a4
Fernando Arruda <fernandojs006@gmail.com>
2022-05-17 00:26:54 -0300

    Ajustes nos botões de login

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { Text, Input, IconButton, Icon, Checkbox, HStack, Flex, InputGroup, InputRightElement, SlideFade, useDisclosure } from "@chakra-ui/react";
+import { Text, Input, IconButton, Icon, Checkbox, HStack, Flex, Box, SlideFade, useDisclosure } from "@chakra-ui/react";
 import { BsArrowRightShort, BsFacebook, BsApple } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
@@ -37,22 +37,60 @@ const Sidebar = () => {
             setPassword(e.target.value);
           }}
         />
-        <HStack my={2} justifyContent="flex-start" mb={4} spacing={5}>
-          <IconButton colorScheme="facebook" aria-label="Fazer login via Facebook" icon={<Icon w={4} h={4} as={BsFacebook} />}></IconButton>
-          <IconButton
+        <HStack my={2} justifyContent="space-between" mb={4} spacing={5}>
+          <Flex
+            boxShadow="md"
+            aria-label="Fazer login via Facebook"
+            as="button"
+            width="80px"
+            justifyContent="center"
+            borderRadius="md"
+            p={2.5}
+            bg="#1877f2"
+            _hover={{ backgroundColor: "#0966de" }}
+          >
+            <Icon color="white" w={4} h={4} as={BsFacebook} />
+          </Flex>
+          <Flex
+            boxShadow="md"
+            aria-label="Fazer login via Google"
+            as="button"
+            width="80px"
+            justifyContent="center"
+            borderRadius="md"
+            p={2.5}
+            bg="#ffffff"
+            _hover={{ backgroundColor: "#f7f7f7" }}
+          >
+            <Icon w={4} h={4} as={FcGoogle} />
+          </Flex>
+          <Flex
+            boxShadow="md"
+            aria-label="Fazer login via Apple"
+            as="button"
+            width="80px"
+            justifyContent="center"
+            borderRadius="md"
+            p={2.5}
+            bg="#000000"
+            _hover={{ backgroundColor: "#141414" }}
+          >
+            <Icon color="white" w={4} h={4} as={BsApple} />
+          </Flex>
+          {/*      <IconButton
             _hover={{ backgroundColor: "#f7f7f7" }}
             _focusWithin={{ backgroundColor: "#f0f0f0" }}
             colorScheme="whiteAlpha"
             aria-label="Fazer login via Google"
             icon={<Icon w={4} h={4} as={FcGoogle} />}
-          ></IconButton>
-          <IconButton
+          ></IconButton> */}
+          {/*         <IconButton
             _hover={{ backgroundColor: "#141414" }}
             _focusWithin={{ backgroundColor: "#292929" }}
             bg="black"
             aria-label="Fazer login via Apple"
             icon={<Icon color="white" w={4} h={4} as={BsApple} />}
-          ></IconButton>
+          ></IconButton> */}
         </HStack>
 
         <Checkbox fontWeight="semibold" colorScheme="red">
@@ -81,99 +119,6 @@ const Sidebar = () => {
           </Text>
         </SlideFade>
       </form>
-
-      {/*    <form onSubmit={handleSubmit}>
-        <Input
-          bg="#ededed"
-          _hover={{ backgroundColor: "#e7e7e7" }}
-          _focusWithin={{ backgroundColor: "#f9f9f9" }}
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          my={2}
-          maxW="50ch"
-          focusBorderColor="black"
-          placeholder="NOME DE USUÁRIO"
-          type="text"
-        />
-        <InputGroup>
-          <Input
-            bg="#ededed"
-            _hover={{ backgroundColor: "#e7e7e7" }}
-            _focusWithin={{ backgroundColor: "#f9f9f9" }}
-            pr="4.5rem"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            onFocus={e => setPasswordFocus(true)}
-            my={2}
-            maxW="50ch"
-            focusBorderColor="black"
-            placeholder="SENHA"
-            type={hidden ? "password" : "text"}
-          />
-          {passwordFocus && (
-            <InputRightElement onClick={(e: any) => e.stopPropagation()}>
-              <IconButton
-                bg="transparent"
-                _hover={{ backgroundColor: "transparent" }}
-                _focusWithin={{ backgroundColor: "transparent" }}
-                transform="auto"
-                translateY="2"
-                translateX="-1"
-                h="2rem"
-                aria-label={hidden ? "Mostrar conteúdo" : "Esconder conteúdo"}
-                icon={<Icon w={5} h={5} as={hidden ? AiOutlineEyeInvisible : AiOutlineEye}></Icon>}
-                onClick={(e: any) => {
-                  setHidden(!hidden);
-                }}
-              ></IconButton>
-            </InputRightElement>
-          )}
-        </InputGroup>
-
-        <HStack my={2} justifyContent="flex-start" mb={4} spacing={5}>
-          <IconButton colorScheme="facebook" aria-label="Fazer login via Facebook" icon={<Icon w={4} h={4} as={BsFacebook} />}></IconButton>
-          <IconButton
-            _hover={{ backgroundColor: "#f7f7f7" }}
-            _focusWithin={{ backgroundColor: "#f0f0f0" }}
-            colorScheme="whiteAlpha"
-            aria-label="Fazer login via Google"
-            icon={<Icon w={4} h={4} as={FcGoogle} />}
-          ></IconButton>
-          <IconButton
-            _hover={{ backgroundColor: "#141414" }}
-            _focusWithin={{ backgroundColor: "#292929" }}
-            bg="black"
-            aria-label="Fazer login via Apple"
-            icon={<Icon color="white" w={4} h={4} as={BsApple} />}
-          ></IconButton>
-        </HStack>
-
-        <Checkbox fontWeight="semibold" colorScheme="red">
-          Manter Login
-        </Checkbox>
-
-        <Flex py="50%" justifyContent="center" alignItems="center">
-          <IconButton
-            type="submit"
-            isDisabled={username.length < 2 || !password}
-            size="lg"
-            aria-label="Enviar"
-            colorScheme="red"
-            borderRadius="xl"
-            _hover={{ backgroundColor: "interactable.700" }}
-            bg="interactable.600"
-            icon={<Icon w={8} h={8} as={BsArrowRightShort} />}
-          />
-        </Flex>
-        <Text userSelect="none" onClick={onToggle} cursor="pointer" textAlign="center" fontSize="sm" fontWeight="bold" _hover={{ color: "#0c0c0c" }} color="GrayText">
-          NÃO CONSEGUE INICIAR SSESSÃO?
-        </Text>
-        <SlideFade in={isOpen} offsetY="20px">
-          <Text textAlign="center" fontSize="sm" fontWeight="bold" color="GrayText">
-            AINDA BEM!
-          </Text>
-        </SlideFade>
-      </form> */}
     </Flex>
   );
 };
